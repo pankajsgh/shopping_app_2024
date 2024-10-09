6ee853a64e8f831fa6d3fdd5702a85d36958c247
Pankaj Pratap Singh <pankaj@makeitlivemedia.com>
2024-10-09 13:09:53 +0530

    Update app for android update
    Update app for android update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app_2024/config/constants.dart';
-import 'package:shopping_app_2024/config/routes.dart';
 import 'package:shopping_app_2024/screens/splash/splash_screen.dart';
+
+import 'config/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,15 +10,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: kPrimaryColor),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      // home: const MyHomePage(),
       initialRoute: SplashScreen.routeName,
       //initialRoute: InitScreen.routeName,
       routes: routes,
@@ -27,3 +29,19 @@ class MyApp extends StatelessWidget {
 }
 
 
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        color: Colors.red,
+        child: const Center(
+          child: Text("Hi how are you", style: TextStyle(color: Colors.white, fontSize: 40),),
+        )
+      ),
+    );
+  }
+}
